f8be83bc8f3cddeda117bd84a6ed12a702b92552
Manik <tonysins43@gmail.com>
2023-05-24 19:06:49 +0600

    get all menu

--- index.js
+++ index.js
@@ -24,12 +24,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("RainbowFeastDB").collection('menu');
+    const reviewsCollection = client.db('RainbowFeastDB').collection('reviews');
+
+    app.get('/menu', async(req,res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
