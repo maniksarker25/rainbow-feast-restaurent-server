1ac92859873cd93651789c8135677fb6659c81f3
Manik <tonysins43@gmail.com>
2023-05-24 19:08:51 +0600

    get reviews

--- index.js
+++ index.js
@@ -26,10 +26,14 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("RainbowFeastDB").collection('menu');
-    const reviewsCollection = client.db('RainbowFeastDB').collection('reviews');
+    const reviewCollection = client.db('RainbowFeastDB').collection('reviews');
 
     app.get('/menu', async(req,res)=>{
         const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+    app.get('/reviews', async(req,res)=>{
+        const result = await reviewCollection.find().toArray();
         res.send(result)
     })
 
