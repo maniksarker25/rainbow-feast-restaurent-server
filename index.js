7eda9f5a84b8cd84e84b3552e0a55e3bdcabf03e
Manik <tonysins43@gmail.com>
2023-06-01 14:41:16 +0600

    make verify token

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
-// middle ware 
+// middle ware
 app.use(cors());
 app.use(express.json());
 
+// verify token
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.16yxiu9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +39,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,96 +47,134 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db("RainbowFeastDB").collection('users');
-    const menuCollection = client.db("RainbowFeastDB").collection('menu');
-    const reviewCollection = client.db('RainbowFeastDB').collection('reviews');
-    const cartCollection = client.db('RainbowFeastDB').collection('carts');
+    const userCollection = client.db("RainbowFeastDB").collection("users");
+    const menuCollection = client.db("RainbowFeastDB").collection("menu");
+    const reviewCollection = client.db("RainbowFeastDB").collection("reviews");
+    const cartCollection = client.db("RainbowFeastDB").collection("carts");
 
-    // SECURE APIS 
-    app.post('/jwt', (req,res) =>{
+    // SECURE APIS
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-      res.send({token})
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '2h',
+      });
+      res.send({ token });
+    });
 
     //user related apis
 
+     // Warning: use verifyJWT before using verifyAdmin
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
+    /**
+     * 0. do not show secure links to those who should not see the links
+     * 1. use jwt token: verifyJWT
+     * 2. use verifyAdmin middleware
+     */
+
     // get all users
-    app.get('/users', async(req,res)=>{
+    app.get("/users", verifyJWT,verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
+    });
 
     // add a user
-    app.post('/users', async(req,res)=>{
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log(user)
-      const query = {email:user.email};
+      // console.log(user);
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
-      console.log(existingUser)
-      if(existingUser){
-        return res.send({message:'user already exists'})
+      // console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
+    });
 
-    // make admin users 
-    app.patch('/users/admin/:id', async(req,res) =>{
+    // check admin
+    // secure layer verify jwt
+    // email same
+    // then check admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    // make admin users
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-        role:'admin'
+          role: "admin",
         },
       };
-      const result = await userCollection.updateOne(query,updateDoc);
+      const result = await userCollection.updateOne(query, updateDoc);
       res.send(result);
-    })
-
-
+    });
 
     //menu related apis--------
-    app.get('/menu', async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
     //review related apis---------------
-    app.get('/reviews', async(req,res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     // cart collection  apis
-    app.get('/carts', async(req,res)=>{
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([]);
       }
-      else{
-        const query = {email:email};
-        const result = await cartCollection.find(query).toArray();
-        res.send(result)
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
-    })
-    app.post('/carts', async (req,res)=>{
+
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
       const item = req.body;
-      console.log(item)
+      // console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.delete('/carts/:id', async(req,res)=>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.send(result)
-    })
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -123,12 +182,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Rainbow Feast in setting ");
+});
 
-app.get('/', (req,res)=>{
-    res.send('Rainbow Feast in setting ')
-})
-
-
-app.listen(port,()=>{
-    console.log(`Rainbow Feast setting on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Rainbow Feast setting on port:${port}`);
+});
