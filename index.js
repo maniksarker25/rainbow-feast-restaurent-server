2fcbd551ef4a8e29ebb4ef3c6c76056d1ea2a60d
Manik <tonysins43@gmail.com>
2023-06-01 16:35:35 +0600

    secure post menu api

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run() {
     });
 
     // add a user
-    app.post("/users",verifyJWT,verifyAdmin, async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       // console.log(user);
       const query = { email: user.email };
@@ -134,7 +134,7 @@ async function run() {
       res.send(result);
     });
     
-    app.post('/menu', async(req,res)=>{
+    app.post('/menu',verifyJWT,verifyAdmin, async(req,res)=>{
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
       res.send(result);
