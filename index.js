97ea701eaddbe4a7cc3fefbc532921fd3a1d9459
Manik <tonysins43@gmail.com>
2023-06-02 20:08:14 +0600

    delete payment menu with payments route

--- index.js
+++ index.js
@@ -201,10 +201,12 @@ async function run() {
     })
 
     // payment related api
-    app.post('/payments', async(req,res)=>{
+    app.post('/payments', verifyJWT, async(req,res)=>{
       const payment = req.body;
-      const result = await paymentCollection.insertOne(payment);
-      res.send(result)
+      const insertResult = await paymentCollection.insertOne(payment);
+      const  query = {_id: {$in: payment.cartItems.map(id=> new ObjectId(id))}};
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({insertResult,deleteResult})
     })
 
     // Send a ping to confirm a successful connection
